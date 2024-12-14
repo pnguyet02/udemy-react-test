130aa1831dc87e97f65b2cf6d9aa9fe893a3e693
pnguyet02 <nguyetpt2003@gmail.com>
2024-12-14 23:27:23 +0700

    Them model add new

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,31 @@ import "./App.scss";
 import Header from "./components/Header";
 import TableUsers from "./components/TableUsers";
 import Container from "react-bootstrap/Container";
-
+import ModalAddNews from "./components/ModalAddNew";
+import { useState } from "react";
 function App() {
+  const [isShowModalAddNew, setIsShowModalAddNew] = useState(false);
+  const handleClose = () => {
+    setIsShowModalAddNew(false);
+  };
   return (
     <div className="app-container">
       <Header />
       <Container>
+        <div className="my-3 add-new">
+          <span>
+            <b>List Users:</b>
+          </span>
+          <button
+            className="btn btn-success"
+            onClick={() => setIsShowModalAddNew(true)}
+          >
+            ADD new user
+          </button>
+        </div>
         <TableUsers />
       </Container>
+      <ModalAddNews show={isShowModalAddNew} handleClose={handleClose} />
     </div>
   );
 }
